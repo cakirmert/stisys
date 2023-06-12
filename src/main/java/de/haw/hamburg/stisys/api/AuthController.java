135a337bcc09468354323dc4569331ef478517b0
Mert <66678730+cakirmert@users.noreply.github.com>
2023-06-12 15:54:20 +0200

    a

--- src/main/java/de/haw/hamburg/stisys/api/AuthController.java
+++ src/main/java/de/haw/hamburg/stisys/api/AuthController.java
@@ -4,11 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.servlet.view.RedirectView;
 
 import de.haw.hamburg.stisys.core.ControlledObject;
 import jakarta.servlet.http.HttpSession;
-import jakarta.servlet.http.HttpServletRequest;
 
 import java.util.Map;
 
@@ -19,36 +19,48 @@ public class AuthController {
     @Autowired
     private ControlledObject userService;
 
+    @GetMapping("/login")
+    public String loginPage() {
+        return "login-page";
+    }
+
     @PostMapping("/login")
-    public RedirectView login(@RequestParam String username, @RequestParam String password, HttpServletRequest request) {
+    public RedirectView login(@RequestParam String username, @RequestParam String password, HttpSession session) {
         Map<String, Object> roleAndId = userService.authenticateUser(username, password);
 
         if (roleAndId != null) {
             String role = String.valueOf(roleAndId.get("role"));
             String userId = String.valueOf(roleAndId.get("userId"));
 
-            HttpSession session = request.getSession();
             session.setAttribute("userId", userId);
             session.setAttribute("role", role);
 
-            switch (role) {
-                case "FSB":
-                    return new RedirectView("/fsb");
-                case "Professor":
-                    return new RedirectView("/professor");
-                case "Student":
-                    return new RedirectView("/student");
-                default:
-                    return new RedirectView("/error?errorMessage=Unknown role");
-            }
+            return new RedirectView("/api/home");
         } else {
-            return new RedirectView("/error");
+            return new RedirectView("/error?errorMessage=Invalid credentials");
         }
     }
 
-    @GetMapping("/error")
-    public String errorPage(Model model, @RequestParam(required = false) String errorMessage) {
-        model.addAttribute("errorMessage", errorMessage);
-        return "error-page";
+    @GetMapping("/home")
+    public String homePage(HttpSession session, RedirectAttributes redirectAttributes) {
+        String role = (String) session.getAttribute("role");
+    
+        if (role != null) {
+            switch (role) {
+                case "FSB":
+                    return "redirect:/fsb";
+                case "Professor":
+                    return "redirect:/professor";
+                case "Student":
+                    return "redirect:/student";
+                default:
+                    redirectAttributes.addAttribute("errorMessage", "Unknown role");
+                    return "redirect:/error";
+            }
+        } else {
+            redirectAttributes.addAttribute("errorMessage", "Not logged in");
+            return "redirect:/error";
+        }
     }
+    
 }
